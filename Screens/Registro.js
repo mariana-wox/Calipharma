aa2e9fc67ddcd6b3ae2d9f88e3f3fb71d6726ec3
mariana-wox <iandreamariana@gmail.com>
2022-06-01 22:30:30 -0500

    Changes donacion

--- Screens/Registro.js
+++ Screens/Registro.js
@@ -1,77 +1,95 @@
 import * as React from "react";
-import { Box, Text, Heading, VStack, FormControl, Input, Link, Button, HStack, Center, NativeBaseProvider } from "native-base";
+import {Box, Text, Heading, VStack, FormControl, Input, Link, Button, 
+  HStack, Center, NativeBaseProvider } from "native-base";
 import { View, Image, StyleSheet } from "native-base";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useNavigation } from '@react-navigation/native';
-import {useState} from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 import { auth } from "../firebase/config";
-import { SafeAreaView } from 'react-native-web';
-function NuevoUs  () {
+import { SafeAreaView } from "react-native-web";
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
+function NuevoUs() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const navigation = useNavigation();
 
   const handleSignup = () => {
     auth
-    .createUserWithEmailAndPassword(email,password)
-    .then(userCredentials=>{
-      const user = userCredentials.user;
-      console.log("Usuario registrado",user.email);
-    }) 
-    .catch(error => alert(error.message))
-  }
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log("Usuario registrado", user.email);
+      })
+      .catch((error) => alert(error.message));
+  };
 
-  return ( 
+  return (
     <Center w="100%">
-    <Box safeArea p="2" py="8" w="90%" maxW="500">
-      
-   <div align= "center">  <Image style={{ width: 303, height: 108, marginBottom: 15 }} source={require("../LogoO.png")}/>
- 
-      </div>
-      
-      <div align= "center">
-      <Heading   size="lg" fontWeight="600" color="coolGray.800" _dark={{
-      color: "warmGray.50"
-    }}>
-          Registrate
-        </Heading>
+      <Box safeArea p="2" py="8" w="90%" maxW="500">
+        <div align="center">
+          {" "}
+          <Image
+            style={{ width: 303, height: 108, marginBottom: 15 }}
+            source={require("../LogoO.png")}
+          />
         </div>
-        <div align= "Center">
-        <Heading mt="1" _dark={{
-        color: "warmGray.200"
-      }} color="coolGray.600" fontWeight="medium" size="xs">
-          Log in
-        </Heading>
+
+        <div align="center">
+          <Heading
+            size="lg"
+            fontWeight="600"
+            color="coolGray.800"
+            _dark={{
+              color: "warmGray.50",
+            }}
+          >
+            Registrate
+          </Heading>
+        </div>
+        <div align="Center">
+          <Heading
+            mt="1"
+            _dark={{
+              color: "warmGray.200",
+            }}
+            color="coolGray.600"
+            fontWeight="medium"
+            size="xs"
+          >
+            Log in
+          </Heading>
         </div>
         <VStack space={3} mt="6">
           <FormControl>
-            <FormControl.Label  >Usuario</FormControl.Label>
-            <Input onChangeText={text=>setEmail(text)}/>
+            <FormControl.Label>Usuario</FormControl.Label>
+            <Input onChangeText={(text) => setEmail(text)} />
           </FormControl>
           <FormControl>
             <FormControl.Label>Contraseña</FormControl.Label>
-            <Input type="password" onChangeText={text=> setPassword(text)}/>
+            <Input type="password" onChangeText={(text) => setPassword(text)} />
           </FormControl>
-          <Button mt="2" colorScheme='emerald' title="Menu" onPress={handleSignup} >
+          <Button
+            mt="2"
+            colorScheme="emerald"
+            title="Menu"
+            onPress={handleSignup}
+          >
             Enviar
           </Button>
         </VStack>
       </Box>
     </Center>
-  );}
+  );
+}
 
-    export default () => {
-        return (
-          <NativeBaseProvider>
-            <Center flex={1} px="3">
-                <NuevoUs/>
-            </Center>
-          </NativeBaseProvider>
-        );
-        
-         
-   };
+export default () => {
+  return (
+    <NativeBaseProvider>
+      <Center flex={1} px="3">
+        <NuevoUs />
+      </Center>
+    </NativeBaseProvider>
+  );
+};
--- Screens/donacion.js
+++ Screens/donacion.js
@@ -3,16 +3,23 @@ import { View, StyleSheet, Text, TouchableOpacity} from "react-native";
 import { Input, CheckBox } from '@rneui/themed';
 
 const donacion = ({ navigation }) => {
+  const [nombre, setNombre] = useState(false);
+  const [ingrediente, setIngrediente] = useState(false);
+  const [cantidad, setCantidad] = useState(false);
+  const [fecha, setFecha] = useState(false);
+  const [presentacion, setPresentacion] = useState(false);
   const [check1, setCheck1] = useState(false);
+
+
   return (
     <View style={styles.container}>
     <Text style={{ color: 'black', fontSize: 20, fontWeight: 'bold', paddingTop: 10, paddingBottom: 10, textAlign: "center" }}> Donando a </Text>
     <Text style={{ color: 'black', fontSize: 17, paddingBottom: 10, textAlign: "center" }}> Fundación Medicina para Todos </Text>
-    <Input style={{marginTop:10, fontSize: 15}} placeholder='Nombre'/>
-    <Input style={{fontSize: 15}} placeholder='Ingrediente Activo'/>
-    <Input style={{fontSize: 15}} placeholder='Cantidad por caja/envase'/>
-    <Input style={{fontSize: 15}} placeholder='Fecha de caducidad'/>
-    <Input style={{fontSize: 15}} placeholder='Presentación'/>
+    <Input onChangeText={(text) => setNombre(text)} style={{marginTop:10, fontSize: 15}} placeholder='Nombre'/>
+    <Input onChangeText={(text) => setIngrediente(text)} style={{fontSize: 15}} placeholder='Ingrediente Activo'/>
+    <Input onChangeText={(text) => setCantidad(text)} style={{fontSize: 15}} placeholder='Cantidad por caja/envase'/>
+    <Input  onChangeText={(text) => setFecha(text)} style={{fontSize: 15}} placeholder='Fecha de caducidad'/>
+    <Input onChangeText={(text) => setPresentacion(text)} style={{fontSize: 15}} placeholder='Presentación'/>
     <CheckBox
       checkedColor="black"
       size={25}
